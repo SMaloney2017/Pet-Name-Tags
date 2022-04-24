f25dc58cd6f8a05d1111798c160b55855704fa4b
SMaloney2017 <SMaloney2017@my.fit.edu>
2022-04-23 20:17:01 -0400

    Removed example-template code

--- src/main/java/com/petnametags/PetNameTagsPlugin.java
+++ src/main/java/com/petnametags/PetNameTagsPlugin.java
@@ -3,9 +3,7 @@ package com.petnametags;
 import com.google.inject.Provides;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
-import net.runelite.api.GameState;
 import net.runelite.api.events.GameStateChanged;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
@@ -28,22 +26,19 @@ public class PetNameTagsPlugin extends Plugin
 	@Override
 	protected void startUp() throws Exception
 	{
-		log.info("Example started!");
+		/* log.info("Example started!"); */
 	}
 
 	@Override
 	protected void shutDown() throws Exception
 	{
-		log.info("Example stopped!");
+		/* log.info("Example stopped!"); */
 	}
 
 	@Subscribe
 	public void onGameStateChanged(GameStateChanged gameStateChanged)
 	{
-		if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
-		{
-			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Example says " + config.greeting(), null);
-		}
+
 	}
 
 	@Provides
